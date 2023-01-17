b98108e15014720287b9d8d38c1d3e2257acb01c
Lucas Martins <lucas2zonta@hotmail.com>
2023-01-17 10:17:44 -0300

    Adicionado a função de desenho no Canvas

--- script.js
+++ script.js
@@ -2,15 +2,51 @@
 let currentColor = 'black';
 let screen = document.querySelector('#tela');
 let ctx = screen.getContext('2d');
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 //Events
 document.querySelectorAll('.color').forEach(item => {
     item.addEventListener('click', colorClick);
 });
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
 
 //Functions
 function colorClick(e){
     currentColor = e.target.getAttribute('data-color');
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
+}
+
+function mouseDownEvent(e) {
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+}
+function mouseMoveEvent(e){
+    if(canDraw) {
+        draw(e.pageX, e.pageY);
+    }
+}
+function mouseUpEvent() {
+    canDraw = false;
+}
+function draw(x, y){
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.lineJoin = 'round';
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
 }
